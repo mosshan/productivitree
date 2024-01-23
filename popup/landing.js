c2228e4357a35bb7caf2d48170d035e612a2d06e
mosshan <mosshan@yahoo.com>
2024-01-22 18:32:55 -0800

    Cleaned up landing, adding canvas w temp code
    Moved around starting states to make code more logically readable
    - Added canvas which will generate tree and base tree code

--- popup/landing.js
+++ popup/landing.js
@@ -5,17 +5,19 @@ sessionComplete = document.getElementById("sessionComplete")
 let getState = browser.storage.local.get("state")
 getState.then(determineStart)
 
+// Show correct state
 function determineStart(item){
-  if(item === undefined || item["state"] == "notInSession"){
+  if(item === undefined || item["state"] === undefined || item["state"] == "notInSession"){
     landing()
   } else if(item["state"] == "inSession"){
-    session()
-  } else{
-    console.log(item["state"])
+    inSession()
+  } else {
     sessionDone()
-  }
+  } 
 }
 
+
+// Base state, no sessions currently active
 function landing(){
   browser.storage.local.set({"state" : "notInSession"})
   landingContent.style.display = 'block'
@@ -24,18 +26,7 @@ function landing(){
 
   startSession = document.getElementById("startSession")
   startSession.addEventListener("click", start)
-
-
 }
-
-function session(){
-  landingContent.style.display = 'none'
-  currentSession.style.display = 'block'
-  sessionComplete.style.display = 'none'
-  endSession = document.getElementById("endSession")
-  endSession.addEventListener("click", sessionDone)
-}
-
 
 // Start a new session
 function start(){
@@ -44,9 +35,21 @@ function start(){
   let alarm = browser.alarms.create("sessionTimer", {delayInMinutes})
   browser.alarms.onAlarm.addListener(sessionDone)
   browser.storage.local.set({"state" : "inSession"})
-  session()
+  inSession()
 }
 
+// Session started, display current session
+function inSession(){
+  landingContent.style.display = 'none'
+  currentSession.style.display = 'block'
+  sessionComplete.style.display = 'none'
+  drawTree(300, 500, -90, depth, 7)
+  endSession = document.getElementById("endSession")
+  endSession.addEventListener("click", sessionDone)
+}
+
+
+// At the end of a session let user determine what to do with their tree
 function sessionDone(){
   browser.storage.local.set({"state" : "sessionComplete"})
   returnToLanding = document.getElementById("return")
@@ -55,6 +58,7 @@ function sessionDone(){
   sessionComplete.style.display = 'block'
 }
 
+// After ending a session and determining what to do with your tree, go back to landing
 function reset(){
   browser.storage.local.set({"state" : "notInSession"})
   landing()
