1a29862723b43c0809417a08742fd5085f3fa2d8
mosshan <mosshan@yahoo.com>
2024-02-23 14:06:17 -0800

    Extension opens in popup window and logically allows only one popup of extension to be opened at once

--- client/src/background/index.ts
+++ client/src/background/index.ts
@@ -1,16 +1,32 @@
-import { isConstructorDeclaration } from 'typescript';
 import { runtime } from 'webextension-polyfill'
 
 let state;
 let timerLength;
-let i = 0
 
 runtime.onInstalled.addListener(() => {
+  chrome.storage.local.set({"popupOpened":"false"})
   console.log('[background] loaded ')
   chrome.storage.local.set({"state":"Landing"})
   state = "Landing"
   console.log('state set to landing ')
 })
+
+
+chrome.action.onClicked.addListener( async () => {
+  let popupURL = chrome.runtime.getURL("index.html")
+  let tabs = await chrome.tabs.query({url: popupURL})
+  if(tabs[0] === undefined){
+    chrome.windows.create({ 
+      url: popupURL,
+      type: "popup"
+    });
+  } else {
+    console.log("tab existas")
+    tabs.forEach(function(tab) {
+      console.log("tab is" + tab + "tab url is " + tab.url + "tab pending url is " + tab.pendingUrl)
+    })
+  }
+});
 
 chrome.runtime.onMessage.addListener(
   function(request, sender){
